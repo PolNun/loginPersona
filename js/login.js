a6474ccb55c98f0f2094442e8e7569376d252cc9
Pablo Nunez <polnunez@outlook.com>
2023-05-27 17:57:44 -0300

    TP 1 funcionando

--- js/login.js
+++ js/login.js
@@ -3,6 +3,7 @@ const inputUsername = document.getElementById('input_username');
 const inputPassword = document.getElementById('input_password');
 const divErrorMessage = document.getElementById('error_message');
 const baseUrl = 'http://localhost:8081';
+const users = [];
 
 btnLogin.addEventListener('click', async () => {
     const response = await fetch(`${baseUrl}/login`, {
@@ -17,6 +18,24 @@ btnLogin.addEventListener('click', async () => {
     });
 
     if (response.ok) {
-        window.location.href = 'inicio.html';
+        await getAllUsers();
+        users.forEach(user => {
+            if (user.user === inputUsername.value) {
+                if (user.role === 'user')
+                    window.location.href = 'inicio.html';
+                else
+                    window.location.href = 'admin.html';
+            }
+        });
     }
 });
+
+async function getAllUsers() {
+    await fetch(`${baseUrl}/getAll`)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(user => {
+                users.push(user);
+            });
+        });
+}
